9322b93bc862283dbf4deb2ccd78358ecbce7f8f
NastyaGavrichenkova <lady_markos@mail.ru>
2023-09-30 13:49:11 +0800

    change test names

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -4,14 +4,14 @@
 from selene import browser, have
 
 
-def test_authorization_desktop(window_size_desktop):
+def test_open_authorization_page_desktop(window_size_desktop):
     browser.open('')
     browser.element('[class*=sign-in]').click()
 
     browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
 
 
-def test_authorization_mobile(window_size_mobile):
+def test_open_authorization_page_mobile(window_size_mobile):
     browser.open('')
     browser.element('.HeaderMenu-toggle-bar').click()
     browser.element('[class*=sign-in]').click()
